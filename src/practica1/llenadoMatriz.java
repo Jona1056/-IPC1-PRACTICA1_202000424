6a3f926f0987695ba6cd4c8d612fedbe6d6a3815
Jonatan <Garciajonatan56@gmail.com>
2022-02-05 00:41:00 -0600

    IMPRENSION DE MATRIZ

--- src/practica1/llenadoMatriz.java
+++ src/practica1/llenadoMatriz.java
@@ -1,6 +1,7 @@
 
 package practica1;
 
+import java.util.Random;
 import static practica1.MATRIZ.columna;
 import static practica1.MATRIZ.fila;
 
@@ -9,29 +10,41 @@ import static practica1.MATRIZ.fila;
  * @author Jonatan Garcia
  */
 public class llenadoMatriz {
-    
+ 
       public String[][] TABLERO(){
+          
        int[][] TableroID = new int[fila][columna];//dimensiones del tablero
        String[][] Tablero = new String[fila][columna];//Tablero para el pacman y 
-       
-      //llenado de la matriz
-      int x = fila*columna;// guardamos en la variable x el numero total espacios en el tablero
-      for(int i = 0; i<TableroID.length; i++){
-        for(int j = 0 ; j>TableroID[0].length; j++){//
-            TableroID[i][j] = x;//guarda el numero de espacios que habran en el tablero
-            x--;
-        }
-    }//imprimir matriz
+       Random random = new Random();
+
+            
+            
+     int NR = 0;
+   
+//imprimir matriz
+    System.out.println("JUGADOR: "+ MATRIZ.nombre);
     System.out.println("----------------------------");
     int c = 0;
     for(int i = 0; i <TableroID.length; i++){ 
-        for(int j = 0; j<TableroID[0].length;j++){
+        for(int j = 0; j<TableroID[0].length;j++){  
+            
+        
+            do{
+                i = random.nextInt(fila);
+                j = random.nextInt(columna);
+               if(Tablero[i][j] ==" #"){
+                   
+               }else
            
-            System.out.print(c +  " ");
-            c++;
-            }
+                    Tablero[i][j] = "@ ";
+                    System.out.print(Tablero[i][j]);
+         
+                c++;
+            }while(c==MATRIZ.a);
+                        }
         System.out.println("");
-    }
+        }
+    
     System.out.println("----------------------------");
     //
         return null;// indicar que salimos del metodo
